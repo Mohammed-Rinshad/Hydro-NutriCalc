6d5c480b78edd9162d4176883a89aab3c7d906d7
Mohammed-Rinshad <shahirabanu97@gmail.com>
2024-08-24 21:14:57 +0530

    added: Route and basename

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter , Route, Routes } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Routes>
+        <Route path='*' element={<App />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
