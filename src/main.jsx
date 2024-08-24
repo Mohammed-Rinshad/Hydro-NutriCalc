0fb339c9f2aa5452d6627ca01f76c9345ea0437d
Mohammed-Rinshad <shahirabanu97@gmail.com>
2024-08-24 21:17:49 +0530

    added: Route and basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter , Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename='/Hydro-NutriCalc/'>
       <Routes>
         <Route path='*' element={<App />}>
         </Route>
